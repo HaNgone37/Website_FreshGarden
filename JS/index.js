a431a9937d9af0024373f7605285246a4879591b
HaNgo <vongtien520@gmail.com>
2024-06-14 12:57:06 +0700

    Cap nhat

--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,5 @@
 plant = document.querySelector(".plant");
 plant.onclick = function(){
     navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
+    navBar.classList.toggle("active"); //them/ xoa active o nav-bar
 }
